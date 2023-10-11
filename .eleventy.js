e5ecb6c792a5e457a0edc8a63814d18b01a10cb0
Jalo Moster <jlvmoster@gmail.com>
2023-10-11 06:32:56 -0400

    Add html minifier for production builds

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const htmlmin = require('html-minifier');
+
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
   // Disable automatic use of .gitignore
   eleventyConfig.setUseGitIgnore(false);
@@ -5,11 +8,23 @@ module.exports = function (eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
 
+  // Minify html output for production build
+  process.env.NODE_ENV === 'production' &&
+    eleventyConfig.addTransform('htmlmin', async function (content) {
+      if (this.page.outputPath && this.page.outputPath.endsWith('.html')) {
+        return htmlmin.minify(content, {
+          collapseWhitespace: true,
+          removeComments: true,
+          useShortDoctype: true,
+        });
+      }
+      return content;
+    });
+
   return {
     dir: {
       input: 'src',
     },
-    markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
   };
 };
